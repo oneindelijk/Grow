67aa763055c7b17831f144b4e1b8a696b7291a4f
oneindelijk <oneindelijk@gmail.com>
2023-10-02 02:09:31 +0200

    Added fallers.py as a derived class for falling organisms

---
+++ src/organisms/fallers.py
@@ -0,0 +1,10 @@
+from organisms.organic_game_element_v2 import OrganicGameElement
+
+class Faller(OrganicGameElement):
+    def __init__(self, x=0, y=0, z30, init_energy_level=0, init_age=0, init_ageing_factor=1.0, terminal_velocity=0):
+        super().__init__(x=x, y=y, z=z, energy_level=init_energy_level, age=init_age, init_ageing_factor=init_ageing_factor)
+        self.terminal_velocity = terminal_velocity
+
+    def fall(self):
+        # Falling logic for the faller
+        pass
